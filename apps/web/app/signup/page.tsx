4cdb7f662c2f796652d331c334730852a8506957
adarsh946 <adarsh.shukla2724@gmail.com>
2024-12-09 23:47:17 +0530

    signup

--- apps/web/app/signup/page.tsx
+++ apps/web/app/signup/page.tsx
@@ -1,10 +1,16 @@
-import { Button } from "@repo/ui/Button";
+import { Button } from "@repo/ui/button";
+import { Card, CardContent, CardHeader } from "@repo/ui/card";
 
 export default function signup() {
   return (
-    <div>
-      <p>signup</p>
-      <Button />
-    </div>
+    <Card className="flex justify-center items-center">
+      <CardHeader>Sign Up to Continue</CardHeader>
+      <CardContent>
+        <form>
+          <label>Email</label>
+          <input placeholder="Email" type="email" />
+        </form>
+      </CardContent>
+    </Card>
   );
 }
